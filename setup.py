d653d4a724da75a516710c674c09c8eecb02684d
hinest <treverhines@gmail.com>
2015-10-02 16:13:41 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration(None, parent_package, top_path)
+    config.add_extension('bvls',sources=['bvls.pyf','bvls.f90']) 
+    return config
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(name = 'BVLS',
+          configuration=configuration)
+
+
+
